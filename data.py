486e945f967a3809fdbae2b7050d83b67895601b
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-15 12:29:23 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,29 @@
+#Learn different data types of Python
+# 1.Integer(int)
+
+a =10
+print(type(a))
+
+# 2.Floating point (float) 
+b = 3.14
+print(type(b))
+
+# 3. String(str)
+c = "Hello"
+print(type(c))
+
+# 4. list
+d =  [1,2,3]
+print(type(d))
+
+# 5. tuple
+e = (4,5,6)
+print(type(e))
+
+# 6. dictionaries (dict) 
+f = {"name": "Ajay", "age": 25}
+print(type(f))
+
+# 7. set
+g ={"apple", "orange", "banana"}
+print(type(g))
